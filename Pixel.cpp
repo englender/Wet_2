d25f174a21e8bbcfa836dce1fe523c4c8d4d1cf6
NadavOrzech <nadavorzach@gmail.com>
2019-01-11 11:41:07 +0200

    קבצים לא רלוונטים

--- Pixel.cpp
+++
@@ -1,7 +0,0 @@
-#include "Pixel.h"
-
-Pixel::Pixel(int ID):pixel_ID(ID), father_pixel(nullptr) {
-    SuperPixel* super_tmp=new SuperPixel(this);
-    this->super=super_tmp;
-
-}
--- SuperPixel.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by nadav on 1/2/2019.
-//
-
-#include "SuperPixel.h"
-SuperPixel::SuperPixel() : max_lable_score(NO_SCORE){
-    this->tree=new Map_tree<int,int>;
-}
